ea77fa8aae3b7f31b55c506e8999d895cae50bca
Davide Castellani <38205382+DavideC03@users.noreply.github.com>
2021-01-03 16:05:34 +0100

    Created bin/eq.py

---
+++ bin/eq.py
@@ -0,0 +1,19 @@
+"""eq
+"""
+
+__author__ = "help@castellanidavide.it"
+__version__ = "1.0 2021-1-3"
+
+class eq:
+	def __init__ (self):
+		"""Where it all begins
+		"""
+		print(eq.eq())
+	
+	def eq():
+		"""eq first funtion
+		"""
+		return "eq" #for the test (see test_eq.py)
+		
+if __name__ == "__main__":
+	eq()
